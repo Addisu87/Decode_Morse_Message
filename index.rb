c7d884ba704ab50c11f3bfb0c9fd3cccd1bf5b67
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-08-17 18:55:13 +0300

    Create a method to decode an entire word in Morse code

--- index.rb
+++ index.rb
@@ -32,13 +32,15 @@ def decode_char(char)
   @Alphabets[char]
 end
 
-puts decode_char(".-")
+puts decode_char('.-')
 
 
-# def decode_word(word)
-#   chars = word.split
-#   decoded_chars = chars.each do |char|
-#     @new_word +=  decode_char(char)
-#   end
-#   decoded_chars.join
-# end
+def decode_word(word)
+  chars = word.split
+  decoded_chars = chars.map do |char|
+    decode_char(char)
+  end
+  decoded_chars.join
+end
+
+puts decode_word('-- -.--')
