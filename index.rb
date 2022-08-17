2e9391250c3e92522d429c835e34b5c692a57f97
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-08-17 18:48:49 +0300

    Create a method to decode a Morse code character

---
+++ index.rb
@@ -0,0 +1,44 @@
+@Alphabets = {
+  " "=> " ",
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(char)
+  @Alphabets[char]
+end
+
+puts decode_char(".-")
+
+
+# def decode_word(word)
+#   chars = word.split
+#   decoded_chars = chars.each do |char|
+#     @new_word +=  decode_char(char)
+#   end
+#   decoded_chars.join
+# end
